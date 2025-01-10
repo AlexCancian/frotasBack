c5cee2cbcf074221606f80f8f291c9e0e017048a
Alex Cancian <alexangelocancian@gmail.com>
2025-01-10 14:46:52 -0300

    ajustes de horas agendas user

--- src/utils/gerarHoraAtual.ts
+++ src/utils/gerarHoraAtual.ts
@@ -6,4 +6,10 @@ const generateDateTimeISO = (): string => {
   return format(adjustedTime, "yyyy-MM-dd'T'HH:mm:ssXXX");
 };
 
-export default generateDateTimeISO;
+const generateDateISO = (): string => {
+  const now = new Date();
+  const adjustedTime = subHours(now, 3); // Ajusta para UTC-3
+  return format(adjustedTime, "yyyy-MM-dd");
+};
+
+export {generateDateTimeISO, generateDateISO};
